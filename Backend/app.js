5095ac5f99d7eac8263ba0e8cf7b8b8cdd99a479
oneRaSol <67533138+oneRaSol@users.noreply.github.com>
2023-03-15 23:57:44 -0400

    added prefix to uploaded file  fo

--- Backend/app.js
+++ Backend/app.js
@@ -16,7 +16,7 @@ const storage = multer.diskStorage({
         callBack(null, 'uploads')
     },
     filename: (req, file, callBack) => {
-        callBack(null, `FunOfHeuristic_${file.originalname}`)
+        callBack(null, `Rasolbey_${file.originalname}`)
     }
   })
 
